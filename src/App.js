6919d2b7e8e7417d0b4393fe18d54925d653b260
jmeno1011 <jwhltn8282@gmail.com>
2022-05-01 16:32:32 +0900

    App.js add Todos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div>
       <Counter number={0} />
+      <hr />
+      <Todos />
     </div>
   );
 }
