596a5817003482c2ffb37c6781db595396f413a5
jmeno1011 <jwhltn8282@gmail.com>
2022-05-03 00:00:02 +0900

    container component add

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,8 @@
+import Counter from '../components/Counter';
+
+// 컨테이너 컴포넌트 : 리덕스 스토어와 연동된 컴포넌트를 컨테이너 컴포넌트라고 한다.
+const CounterContainer = () => {
+  return <Counter />;
+};
+
+export default CounterContainer;
