44f58c57c9195789da4662b90cf129f6ac2de93d
jmeno1011 <jwhltn8282@gmail.com>
2022-05-03 00:13:46 +0900

    주석 추가

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,8 +1,26 @@
+import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 
 // 컨테이너 컴포넌트 : 리덕스 스토어와 연동된 컴포넌트를 컨테이너 컴포넌트라고 한다.
-const CounterContainer = () => {
-  return <Counter />;
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
 };
 
-export default CounterContainer;
+// mapStateToProps는 리덕스 스토어 안의 상태를 컴포넌트의 props로 넘겨주기 위한 함수
+const mapStateToProps = (state) => ({
+  number: state.counter.number,
+});
+
+// mapDispatchToProps는 액션 생성 함수를 컴포넌트의 props로 넘겨주기 위한 함수
+const mapDispatchToProps = (dispatch) => ({
+  increase: () => {
+    console.log('increase');
+  },
+  decrease: () => {
+    console.log('decrease');
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
